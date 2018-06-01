fb2f73d06ffa945f79e0231d1b34be12d500ac7b
HebaMamdouh93 <hebamamdouh2016@gmail.com>
2018-06-01 21:41:08 +0200

    finish routes of latest&recommended books

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,18 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    resources :categories
+    resources :books do
+      #/api/books/route_name
+      collection do
+        get :latest_books
+        get :recommended_books
+      end
+      #/api/books/:id/route_name
+      member do
+        
+      end
+    end
+   end 
 end
