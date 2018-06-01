871bfacba0f74f58e85bb6d96cbabce35d272fc3
Iman Ragab <iman.mansour94@gmail.com>
2018-06-01 16:50:26 +0200

    make application controller inherit from base - Added protection against XSRF attacks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+    include ActionController::Serialization  
+    protect_from_forgery :with => :exception
 end
