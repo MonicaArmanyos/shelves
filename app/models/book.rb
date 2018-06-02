33bd06c41ca36cf6ae2db7ca3b4d91b3f36077fb
Iman Ragab <iman.mansour94@gmail.com>
2018-06-02 15:43:47 +0200

    accepts nested attributes for book images in book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,11 +2,13 @@ class Book < ApplicationRecord
     enum transcation: {"Sell" => 0, "Free Share" =>1,"Exchange" =>2,"Sell By Bids" =>4}
     
     #### Relations ####
-belongs_to :category
-belongs_to :user
-has_many :book_images
+    belongs_to :category
+    belongs_to :user
+    has_many :book_images
 
-#### Mount image uploader
-mount_uploaders :images, ImageUploader
+    #### accept apload multiple images
+    accepts_nested_attributes_for :book_images
 
 end
+
+
