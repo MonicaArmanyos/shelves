7d1cfc19a0f6f441ac98372483e19440b31634dd
Iman Ragab <iman.mansour94@gmail.com>
2018-06-03 19:57:20 +0200

    add current user to book - books controller

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,5 +1,5 @@
 module Api
-    class Api::BooksController < ApplicationController
+    class Api::BooksController < ApiController
 
          #### Latest Books in Home Page ####
          def latest_books
@@ -29,14 +29,18 @@ module Api
 
         #### Create book 
         def create
-            @book = Book.new(book_params)
-            if @book.save
-                params[:book][:book_images_attributes].each do |file|
-                    @book.book_images.create!(:image => file)
+            if current_user
+                @user = current_user
+                @book = Book.new(book_params)
+                @book.user_id = @user.id
+                if @book.save
+                    params[:book][:book_images_attributes].each do |file|
+                        @book.book_images.create!(:image => file)
+                    end
+                    render json: {status: 'SUCCESS', message: 'Book successfully created', book:@book},status: :ok
+                else
+                    render json: {status: 'FAIL', message: 'Couldn\'t create book', error:@book.errors},status: :ok
                 end
-                render json: {status: 'SUCCESS', message: 'Book successfully created', book:@book},status: :ok
-            else
-                render json: {status: 'FAIL', message: 'Couldn\'t create book', error:@book.errors},status: :ok
             end
         end
 
@@ -44,7 +48,7 @@ module Api
         #### Permitted book params 
         def book_params
             params.require(:book).permit(:name, :description, :transcation, :quantity, 
-                                        :bid_user, :user_id, :category_id, :price, book_images_attributes:[:id, :book_id, :image])
+                                        :bid_user, :category_id, :price, book_images_attributes:[:id, :book_id, :image])
         end
     end
 end
