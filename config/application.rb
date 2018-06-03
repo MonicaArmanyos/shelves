cd238cf860f4a0d6e5381d57aaf79957bb6231ca
Iman Ragab <iman.mansour94@gmail.com>
2018-06-03 03:07:57 +0200

    enable CORS

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,13 @@ module Shelves
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = false
+    # enable CORS
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
   
 end
