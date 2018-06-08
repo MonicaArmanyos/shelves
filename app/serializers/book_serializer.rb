fc8a69fbd2e08546887ebd2983e222a06bec1619
HebaMamdouh93 <hebamamdouh2016@gmail.com>
2018-06-08 13:46:36 +0200

    modify the foreign key user object to skip unused fields

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -4,13 +4,13 @@ class BookSerializer < ActiveModel::Serializer
  #### get bid_user object
   def user_bid
     id_biduser=object.bid_user
-    User.where(:id => id_biduser).select(User.column_names - ["password_digest"])
+    User.where(:id => id_biduser).select(User.column_names - ["password_digest","email_confirmed","confirm_token","created_at","updated_at","password_reset_token","password_reset_sent_at"])
 
   end 
 
   def user
     id_user=object.user_id
-    User.where(:id => id_user).select(User.column_names - ["password_digest"])
+    User.where(:id => id_user).select(User.column_names - ["password_digest","email_confirmed","confirm_token","created_at","updated_at","password_reset_token","password_reset_sent_at"])
 
   end 
 end
