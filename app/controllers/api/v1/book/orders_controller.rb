5b5c799166e1ac33f198619dba5cc3eb592419dc
ghadakhamis <ghada.khamis.m@gmail.com>
2018-06-08 22:40:05 +0200

    add comments to order controller

--- app/controllers/api/v1/book/orders_controller.rb
+++ app/controllers/api/v1/book/orders_controller.rb
@@ -5,17 +5,22 @@ module Api::V1::Book
     def create
       @current_user = AuthorizeApiRequest.call(request.headers).result
       if @current_user
+        # check if book is available and approved or not
         if ((@book.is_available.eql? true) && (@book.is_approved.eql? true)) 
+          # check book transcation
           if @book.transcation.eql? "Free Share"
             @order = @book.orders.new(:book_id => @book.id, :user_id => @current_user.id, :state => 0, :seller => @book.user_id, :transcation => @book.transcation, :price => @book.price)
           elsif @book.transcation.eql? "Sell"
+            # check if quantity exist, larger than zero and less than book quantity 
             if ((params[:quantity]) && (params[:quantity].to_i < @book.quantity) && (params[:quantity].to_i > 0))
               @order = @book.orders.new(:book_id => @book.id, :user_id => @current_user.id, :state => 0, :seller => @book.user_id, :transcation => @book.transcation, :price => @book.price * params[:quantity].to_i, :quantity => params[:quantity].to_i)
             else
               render json: {status: 'FAIL', message: 'This Quantity not valid', error:@book.errors},status: :ok
             end  
           end  
+          # check if order created or not
           if @order
+            # save order and check it saved successfuly
             if @order.save
               ###notification to book owner
               render json: {status: 'SUCCESS', message: 'order successfully created', order: @order},status: :ok
