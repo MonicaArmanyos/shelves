178a901439d7219438b70be68ba6e71a70ac802f
MonicaArmanyos <monicamagdy70@gmail.com>
2018-06-08 20:44:09 +0200

    enhancing show and authenticate user

--- app/controllers/api/v1/user/authentication_controller.rb
+++ app/controllers/api/v1/user/authentication_controller.rb
@@ -1,27 +1,29 @@
-class Api::V1::User::AuthenticationController < ApiController
-    skip_before_action :authenticate_request, only: :authenticate
+module Api::V1::User
+  class Api::V1::User::AuthenticationController < ApiController
+      skip_before_action :authenticate_request, only: :authenticate
 
-    def authenticate
-      command = AuthenticateUser.call(params[:email], params[:password])
-   
-      if command.success?
-        @user= User.find_by_email(params[:email])
-        if  @user.email_confirmed
-          if params[:remember_me]
-            
-          else
-            
+      def authenticate
+        command = AuthenticateUser.call(params[:email], params[:password])
+    
+        if command.success?
+          @user= User.find_by_email(params[:email])
+          if  @user.email_confirmed
+            if params[:remember_me]
+              
+            else
+              
+            end
+            render json: {user: @user, auth_token: command.result }, :except => [:password_digest], status: :created
+          else 
+            render json: :inactive
           end
-          render json: {user: @user, auth_token: command.result }, status: :created
-        else 
-          render json: :inactive
+        else
+          render json: { error: command.errors }, status: :unauthorized
         end
-      else
-        render json: { error: command.errors }, status: :unauthorized
       end
-    end
 
 
 
-  
+    
+  end
 end
