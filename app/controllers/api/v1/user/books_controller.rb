ea42ffca7c2b1a471d78a51b46c0bb98850131a4
Iman Ragab <iman.mansour94@gmail.com>
2018-06-10 00:06:26 +0200

    modify user books controller

--- app/controllers/api/v1/user/books_controller.rb
+++ app/controllers/api/v1/user/books_controller.rb
@@ -1,11 +1,11 @@
 module Api::V1::User
-    class Api::V1::User::BooksController < ApiController 
-  
+    class Api::V1::User::BooksController < ApplicationController
+         
         def show
             user = User.find(params[:id])
             @books = user.books
             if(@books)
-                render json: {status: 'SUCCESS', books:@books},status: :ok
+                render json: {status: 'SUCCESS', books:@books, each_serializer: BookSerializer},status: :ok
             else
                 render json: {status: 'FAil', message: 'Can\'t get user\'s books!'},status: :ok
             end
