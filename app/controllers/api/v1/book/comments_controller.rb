f9ba239f3cc46c1bb3fa70eae301b8cdc16cf6ee
ghadakhamis <ghada.khamis.m@gmail.com>
2018-06-12 01:19:52 +0200

    add two functions to find book and authentication user

--- app/controllers/api/v1/book/comments_controller.rb
+++ app/controllers/api/v1/book/comments_controller.rb
@@ -1,5 +1,17 @@
 module Api::V1::Book
-
   class Api::V1::Book::CommentsController < ApplicationController
+   
+    before_action :set_book
+    before_action :authenticate_request
+
+
+    private
+    def set_book
+      @book = Book.find(params[:book_id])
+    end
+    #### Authentication of user ####
+    def authenticate_request
+      @current_user = AuthorizeApiRequest.call(request.headers).result
+    end
   end
 end  
