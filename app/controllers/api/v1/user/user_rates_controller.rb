47b183b03a180aa175802105f908516794b5de72
MonicaArmanyos <monicamagdy70@gmail.com>
2018-06-12 11:05:26 +0200

    user can rate or update their rating on other user

--- app/controllers/api/v1/user/user_rates_controller.rb
+++ app/controllers/api/v1/user/user_rates_controller.rb
@@ -10,7 +10,8 @@ module Api::V1::User
                 
                @user_rate = UserRate.where(user_id: @rated_user.id, rated_by: @current_user.id)
                if @user_rate.any?
-                 if @user_rate.update(rate: params[:rate])
+                @user_rate[0].rate = params[:rate];
+                 if @user_rate[0].update_attributes(id: @user_rate[0].id)
                     @rated_user.rate = @rated_user.user_rates.average(:rate)
                     if @rated_user.save
                         render json: {status: 'SUCCESS', message: @rated_user.name+' has been rated'},status: :ok
