80aa164f793873d85ef0cb8df1716c22f931f59b
ghadakhamis <ghada.khamis.m@gmail.com>
2018-06-13 22:53:10 +0200

    handel comment and reply serializer to return comment with all replies on it

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,6 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :user, :comment, :created_at
+  attributes :id, :user, :comment, :created_at, :replays
+  has_many :replays
   def user
     id_user=object.user_id
     User.where(:id => id_user).select(User.column_names - ["password_digest","email_confirmed","confirm_token","created_at","updated_at","password_reset_token","password_reset_sent_at","email","gender","role","rate"])
--- app/serializers/replay_serializer.rb
+++ app/serializers/replay_serializer.rb
@@ -1,13 +1,8 @@
 class ReplaySerializer < ActiveModel::Serializer
-  attributes :id, :user, :replay, :created_at, :comment
-
+  attributes :id, :user, :replay, :created_at
   def user
     id_user=object.user_id
     User.where(:id => id_user).select(User.column_names - ["password_digest","email_confirmed","confirm_token","created_at","updated_at","password_reset_token","password_reset_sent_at","email","gender","role","rate"])
-  end 
-  def comment
-    comment_id=object.comment_id
-    Comment.where(:id => comment_id).select(Comment.column_names - ["created_at","updated_at","like"])
   end 
   def created_at
     object.created_at = object.created_at.strftime("%B %e, %Y at %I:%M %p")
