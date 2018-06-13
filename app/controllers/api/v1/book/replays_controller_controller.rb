197e4eb6c0d6f9c354d8568974ff8a1862741b7a
ghadakhamis <ghada.khamis.m@gmail.com>
2018-06-13 19:05:08 +0200

    create replay controller

---
+++ app/controllers/api/v1/book/replays_controller_controller.rb
@@ -0,0 +1,5 @@
+module Api::V1::Book
+  class Api::V1::Book::ReplaysController < ApplicationController
+
+  end  
+end
---
+++ test/controllers/api/replays_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::ReplaysControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
