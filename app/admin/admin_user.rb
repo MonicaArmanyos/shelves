d83be0b33e253001af018a2a534c12d3eeb4129a
ghadakhamis <ghada.khamis.m@gmail.com>
2018-06-14 01:23:08 +0200

    custom admin user in active admin

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,16 +1,47 @@
 ActiveAdmin.register AdminUser do
   permit_params :email, :password, :password_confirmation
+  scope :all,default: true
+  scope :created_this_week do |tasks|
+    tasks.where('created_at > ? and created_at < ?', Time.now, 1.week.from_now)
+  end
+  scope :late do |tasks|
+    tasks.where('created_at < ?', Time.now)
+  end
 
+  config.per_page =9
+  
   index do
     selectable_column
     id_column
     column :email
     column :current_sign_in_at
+    column :last_sign_in_at
     column :sign_in_count
     column :created_at
+    column :remember_created_at
     actions
   end
 
+  show do
+    attributes_table do
+      row :email
+      row :remember_created_at
+    end
+    attributes_table do
+      row :current_sign_in_at
+      row :last_sign_in_at
+      row :sign_in_count
+    end 
+    attributes_table do
+      row :current_sign_in_ip
+      row :last_sign_in_ip
+    end  
+    attributes_table do
+      row :created_at
+      row :updated_at
+    end 
+    active_admin_comments 
+  end  
   filter :email
   filter :current_sign_in_at
   filter :sign_in_count
