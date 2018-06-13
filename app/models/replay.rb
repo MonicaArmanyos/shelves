4b1e9206673430212cc7ce5b8c7fcefc230e88b5
ghadakhamis <ghada.khamis.m@gmail.com>
2018-06-13 18:43:47 +0200

    change migrationof replays and add validation in replay model

--- app/models/replay.rb
+++ app/models/replay.rb
@@ -1,4 +1,8 @@
 class Replay < ApplicationRecord
+  
+  #validation
+  validates :replay, :user_id, :comment_id, presence: true
+  validates :replay, length: {minimum:2}
 
   #### Relations ####
   belongs_to :user
