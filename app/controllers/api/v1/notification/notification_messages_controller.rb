7b0adec74d60717c302f6cbc7fafc050cefcec77
HebaMamdouh93 <hebamamdouh2016@gmail.com>
2018-06-14 15:31:30 +0200

    modify in notification_message controller functions apis

--- app/controllers/api/v1/notification/notification_messages_controller.rb
+++ app/controllers/api/v1/notification/notification_messages_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::Notification::NotificationMessagesController < ApplicationController
-    
+    before_action :authenticate_request
     #### get all notification messages for current user
     def get_user_notifications
-        if  NotificationMessage.exists?(:receiver_user => params[:id])
-            @notification_messages= NotificationMessage.where(:receiver_user => params[:id]).where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month)
+        if  NotificationMessage.exists?(:receiver_user => @current_user.id)
+            @notification_messages= NotificationMessage.where(:receiver_user => @current_user.id).where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month)
             render :json => @notification_messages, each_serializer: NotificationMessageSerializer
         else
             render json: {status: 'FAIL', message: 'No Notification messages For this user'},status: :ok
@@ -12,8 +12,8 @@ class Api::V1::Notification::NotificationMessagesController < ApplicationControl
 
     #### update notification message to be seen
     def update_seen_notification
-        if  NotificationMessage.exists?(:id => params[:id])
-            @notification_message = NotificationMessage.find(params[:id])
+        if  NotificationMessage.exists?(:id => params[:id].to_i)
+            @notification_message = NotificationMessage.find(params[:id].to_i)
             @notification_message.update(:is_seen => 1)
             render json: {status: 'SUCCESS', message: 'Notification is seen by user'},status: :ok
         else
@@ -24,14 +24,20 @@ class Api::V1::Notification::NotificationMessagesController < ApplicationControl
 
     #### get all unseen notification messages for current_user 
     def get_no_unseen_notification_messages
-        if  NotificationMessage.exists?(:receiver_user => params[:id])
-            @notification_message = NotificationMessage.find(params[:id]).where(:is_seen => 0)
+        if  NotificationMessage.exists?(:receiver_user => @current_user.id)
+            @notification_message = NotificationMessage.where(:receiver_user => @current_user.id).where(:is_seen => 0)
             @unseen_notification_messages =  @notification_message.count
             render json: {status: 'SUCCESS', message: 'Notification is seen by user', no_unseen_notification_messages: @unseen_notification_messages},status: :ok
         else
             render json: {status: 'FAIL', message: 'No Notification messages For this user'},status: :ok
         end  
     end
-
+    
+    private
+    #### Authentication of user ####
+    def authenticate_request
+        @current_user = AuthorizeApiRequest.call(request.headers).result
+        render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+    end
    
 end
