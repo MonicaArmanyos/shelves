6b66ac1e07e69957c97a9d60a90d3ee22fbb1d25
Iman Ragab <iman.mansour94@gmail.com>
2018-06-14 07:37:21 +0200

    modify login success response json

--- app/controllers/api/v1/user/authentication_controller.rb
+++ app/controllers/api/v1/user/authentication_controller.rb
@@ -9,7 +9,8 @@ module Api::V1::User
           @user= User.find_by_email(params[:email])
           if  @user.email_confirmed
             
-            render json: {status: 'SUCCESS',user: @user, auth_token: command.result }, :except => [:password_digest], status: :created
+            render json: {status: 'SUCCESS',user: @user, books: @user.books, auth_token: command.result, phones: @user.phones, addresses: @user.addresses, interests: @user_interests }, :except => [:password_digest], status: :created
+
           else 
             render json: {status: 'FAIL', message: "This account is inactive! Please check your email to activate your account."}, status: :ok
            
