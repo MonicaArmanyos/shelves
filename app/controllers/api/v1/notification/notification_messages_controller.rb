40cc3fdb0604e68518ef79e3042a6d2b2285fb8d
HebaMamdouh93 <hebamamdouh2016@gmail.com>
2018-06-14 14:47:24 +0200

    finish update seen notification api to update is_seen column when user read notification

--- app/controllers/api/v1/notification/notification_messages_controller.rb
+++ app/controllers/api/v1/notification/notification_messages_controller.rb
@@ -5,11 +5,18 @@ class Api::V1::Notification::NotificationMessagesController < ApplicationControl
             @notification_messages= NotificationMessage.where(:receiver_user => params[:id]).where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month)
             render :json => @notification_messages, each_serializer: NotificationMessageSerializer
         else
-            render json: {status: 'FAIL', message: 'No Notification messages For that user'},status: :ok
+            render json: {status: 'FAIL', message: 'No Notification messages For this user'},status: :ok
         end
     end
 
     def update_seen_notification
+        if  NotificationMessage.exists?(:receiver_user => params[:id])
+            @notification_message = NotificationMessage.find(params[:id])
+            @notification_message.update(:is_seen => 1)
+            render json: {status: 'SUCCESS', message: 'Notification is seen by user'},status: :ok
+        else
+            render json: {status: 'FAIL', message: 'No Notification messages For this user'},status: :ok
+        end  
 
     end
 
