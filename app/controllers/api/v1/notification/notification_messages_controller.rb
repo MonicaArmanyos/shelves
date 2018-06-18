d39605c5fb149ccf2be7ffccee648bdf1a59234f
HebaMamdouh93 <hebamamdouh2016@gmail.com>
2018-06-18 21:10:38 +0200

    add validations on notifications messages

--- app/controllers/api/v1/notification/notification_messages_controller.rb
+++ app/controllers/api/v1/notification/notification_messages_controller.rb
@@ -9,9 +9,13 @@ class Api::V1::Notification::NotificationMessagesController < ApplicationControl
                     @notification_messages= NotificationMessage.where(:receiver_user => @current_user.id).where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month).order("created_at DESC")
                     
                 elsif params[:status] == "navbar-notifications"
-                     @notification_messages= NotificationMessage.where(:receiver_user => @current_user.id).where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month).order("created_at DESC").limit(1)
+                     @notification_messages= NotificationMessage.where(:receiver_user => @current_user.id).where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month).order("created_at DESC").limit(8)
                 end
-                render :json => @notification_messages, each_serializer: NotificationMessageSerializer
+                if @notification_messages.count != 0
+                    render :json => @notification_messages, each_serializer: NotificationMessageSerializer
+                else
+                    render json: {status: 'FAIL', message: 'No Notification messages For this user'},status: :ok
+                end
             else
                 render json: {status: 'FAIL', message: 'No Notification messages For this user'},status: :ok
             end
@@ -22,10 +26,15 @@ class Api::V1::Notification::NotificationMessagesController < ApplicationControl
     def update_seen_notification
         if  NotificationMessage.exists?(:receiver_user => @current_user.id)
             @notification_messages = NotificationMessage.where(:receiver_user => @current_user.id).where(:is_seen => 0)
-            @notification_messages.each do |notification_message|
-                notification_message.update(:is_seen => 1)
+            if @notification_messages.count != 0
+                @notification_messages.each do |notification_message|
+                    notification_message.update(:is_seen => 1)
+                end
+                render json: {status: 'SUCCESS', message: 'Notification is seen by user'},status: :ok
+            else
+                render json: {status: 'FAIL', message: 'No Notification messages for this id'},status: :ok
             end
-            render json: {status: 'SUCCESS', message: 'Notification is seen by user'},status: :ok
+
         else
             render json: {status: 'FAIL', message: 'No Notification messages for this id'},status: :ok
         end  
@@ -37,7 +46,12 @@ class Api::V1::Notification::NotificationMessagesController < ApplicationControl
         if  NotificationMessage.exists?(:receiver_user => @current_user.id)
             @notification_message = NotificationMessage.where(:receiver_user => @current_user.id).where(:is_seen => 0)
             @unseen_notification_messages =  @notification_message.count
-            render json: {status: 'SUCCESS', message: 'Notifications are seen by user', no_unseen_notification_messages: @unseen_notification_messages},status: :ok
+            if @unseen_notification_messages != 0
+                render json: {status: 'SUCCESS', message: 'Notifications are seen by user', no_unseen_notification_messages: @unseen_notification_messages},status: :ok
+            else
+                render json: {status: 'FAIL', message: 'No Notification messages For this user'},status: :ok
+            end
+
         else
             render json: {status: 'FAIL', message: 'No Notification messages For this user'},status: :ok
         end  
